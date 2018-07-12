2ecc8170562e7bfe58594d28514fb06b9a6cb7ad
DungB96 <dungb96@Admins-iMac-2.local>
2018-07-12 15:01:04 +0700

    3TableView

---
+++ 3TableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3TableView
+//
+//  Created by DungB96 on 2018/05/16.
+//  Copyright © 2018 DungB96. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
